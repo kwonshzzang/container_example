ca27feb57493f340178cfca02f911c1af74f5b5d
kwonshzzang <kwonshzzang@gmail.com>
2024-01-17 15:58:28 +0900

    First Commit

---
+++ src/main/java/kr/co/kwonshzzang/containerexample/ContainerExampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.containerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerExampleApplication.class, args);
+    }
+
+}
